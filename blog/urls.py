0e746d6b01d3a074f84c4b4cea3a82b9bdae4f7c
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-08-18 14:12:12 -0700

    add base.html and update views

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.starting_page, name="starting-page"),
+    path("posts/", views.posts_page, name="posts-page"),
+    path("posts/<slug:post_id>/", views.post_details, name="post-details")
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def starting_page(request):
     return render(request, 'blog/index.html')
+
+
+def posts_page(request):
+    return render(request, 'blog/posts-page.html')
+
+
+def post_details(request, post_id):
+    return render(request, 'blog/post-details.html')
